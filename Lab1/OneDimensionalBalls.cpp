744ad36a25df1156fc93bff7227419fde0821a68
Audaris Blades <audaris@gmail.com>
2017-09-16 22:58:49 -0400

    First Successful Compile

--- Lab1/OneDimensionalBalls.cpp
+++ Lab1/OneDimensionalBalls.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <set>
 #include <algorithm>
-
+using namespace std;
 class Ball{
 	public:
 		int val;
@@ -17,25 +17,30 @@ class Ball{
 //Stores all balls in first picture
 map<int, Ball*> FBMAP;
 //Stores all balls in second picture
-map<int Ball*> SBMAP
+map<int, Ball*> SBMAP;
 
-long countValidGuesses(vector<int>  FP, vector<int>  SP){
-	
+long long countValidGuesses(vector<int>  FP, vector<int>  SP){
+
 	//Add Balls from firstPicture to FBMap
 	for(int i = 0; i < FP.size(); i++){
 		//Set Ball attributes
 		Ball cBall;
 		cBall.matched = 0;
-		cBall.low->NULL;
-		cBall.high->NULL;
-		FBMAP.insert(make_pair(FP[i], &cBall);
+		cBall.low = NULL;
+		cBall.high = NULL;
+		FBMAP.insert(make_pair(FP[i], &cBall));
 	}
 
 	//Add Balls from secondPicture to SBMap
 	for(int i = 0; i < SP.size(); i++){
 		Ball cBall;
-		SBMAP.insert(make_pair(SP[i], &cBall);
+		SBMAP.insert(make_pair(SP[i], &cBall));
 	}
-
 	
+	return 0;
 };
+
+
+int main(){
+	return 0;
+}
