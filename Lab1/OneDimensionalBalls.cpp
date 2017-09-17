5a5a832b342294f5ea076273fa3651cbaf671653
Audaris Blades <audaris@gmail.com>
2017-09-17 16:55:16 -0400

    Updated file with smart pointers and rmate integration with sublime

--- Lab1/OneDimensionalBalls.cpp
+++ Lab1/OneDimensionalBalls.cpp
@@ -24,20 +24,36 @@ long long countValidGuesses(vector<int>  FP, vector<int>  SP){
 	//Add Balls from firstPicture to FBMap
 	for(int i = 0; i < FP.size(); i++){
 		//Set Ball attributes
-		Ball cBall;
-		cBall.matched = 0;
-		cBall.low = NULL;
-		cBall.high = NULL;
+		Ball *b;
+		b->matched = 0;
+		b->low = NULL;
+		b->high = NULL;
 		FBMAP.insert(make_pair(FP[i], &cBall));
 	}
 
 	//Add Balls from secondPicture to SBMap
 	for(int i = 0; i < SP.size(); i++){
-		Ball cBall;
+		Ball *b;
+		b->matched = 0;
+		b->low = NULL;
+		b->.high = NULL;
 		SBMAP.insert(make_pair(SP[i], &cBall));
 	}
 	
 	return 0;
+};
+
+long long cvg(int v){
+
+	//Loop through FBMAP
+	for(map<int, Ball*>::iterator it = FBMAP.begin(); it! = FBMAP.end(); it++){
+		//Find  int -val and int +val in the current map
+		//set low and high accourdingly
+		Ball* b;
+		b->it.second;
+		b->low = (it.find(b.val - v)->second);
+		b->high = (it.find(b.val + v)->second);
+	}
 };
 
 
