7620a12feab729e9cca34c0a3efc8489328aa7d4
Audaris Blades <audaris@gmail.com>
2017-09-17 20:13:20 -0400

    commit before compile

--- Lab1/OneDimensionalBalls.cpp
+++ Lab1/OneDimensionalBalls.cpp
@@ -54,6 +54,53 @@ long long cvg(int v){
 		b->high = ((SBMAP.find(b->val + v))->second);
 	}
 
+	//Loop through FSBMAP and assign high and low values to elements in FB
+	for(map<int, Ball*>::iterator it = SBMAP.begin(); it!= SBMAP.end(); it++){
+		//ball in current iteration
+		Ball *b = it->second;
+		//Find adjecent by v elements low and high
+		b->low = ((FBMAP.find(b->val - v))->second);
+		b->high = ((FBMAP.find(b->val + v))->second);
+	}
+
+	//Check all balls in FP If no valid low or high pointer return zero (no guesses)
+	for(map<int, Ball*>::iterator it = FBMAP.begin();it != FBMAP.end();it++){
+		Ball *b = it->second;
+		//There are no matches if there are no pointers!
+		if(b->low == NULL && b->high == NULL)
+			return 0;
+
+	}	//Check all balls in FP again If any ball has 1 low or high only one way to match 
+	for(map<int, Ball*>::iterator it = FBMAP.begin();it != FBMAP.end();it++){
+		Ball *b = it->second;
+		//There are no matches if there are no pointers!
+		//Exclusive or check
+		if((b->low == NULL) != (b->high == NULL)){
+			b->matched = 1;
+			//temp pointer points to child ball to change values;
+			Ball *childBall;
+			if(b->low != NULL)
+				childBall = b->low;
+			else
+				childBall = b->high;
+
+			childBall->matched = 1;
+
+			//set proper high/lowpointer
+			if(childBall->val < b->val)
+				childBall->low = NULL;
+			else
+				childBall->high = NULL;
+
+			//Dereference the childBall
+			childBall = NULL;
+
+
+
+		}
+	}
+
+	//set matched filed that it point to in sb to 1 set appropriate low and high to null for each ball
 
 };
 
