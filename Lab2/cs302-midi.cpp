bab7c9b5542e7a9016fc12c9a3fae01260df7ba7
Audaris Blades <audaris@gmail.com>
2017-09-19 12:03:34 -0400

    Add the midi header file. Update midi.cpp with eventlist loop

--- Lab2/cs302-midi.cpp
+++ Lab2/cs302-midi.cpp
@@ -14,9 +14,27 @@ Convert MIDI Event/Note Files
 #include "cs302-midi.h"
 using namespace std;
 
+//EL is Full
+//Map is empty
 void CS302_Midi::el_to_nd()
 {
   nd = new NDMap;
+  
+  //Indexed by pitch
+  vector<ND*> tmp;
+  
+  //Create Vector of size 128
+  tmp.resize(128);
+
+  //Set Vector Values to NULL
+  for(vector<ND*>::iterator it = tmp.begin(); it != tmp.end(); it++)
+  		tmp.insert(it, NULL);
+
+  //Loop through EventList
+  for(list<Event *>::iterator it = EventList.begin(),it != EventList.end(); it++){
+
+
+  }
 }
 
 void CS302_Midi::nd_to_el()
