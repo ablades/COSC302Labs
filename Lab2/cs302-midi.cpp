fe153b498dd113ee00266f626a2288517601149d
Audaris Blades <audaris@gmail.com>
2017-09-19 11:32:14 -0400

    Add all test files for lab2

--- Lab2/cs302-midi.cpp
+++ Lab2/cs302-midi.cpp
@@ -3,3 +3,23 @@ Lab2
 Convert MIDI Event/Note Files
 */
 
+#include <iostream>
+#include <map>
+#include <vector>
+#include <fstream>
+#include <list>
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include "cs302-midi.h"
+using namespace std;
+
+void CS302_Midi::el_to_nd()
+{
+  nd = new NDMap;
+}
+
+void CS302_Midi::nd_to_el()
+{
+  el = new EventList;
+}
