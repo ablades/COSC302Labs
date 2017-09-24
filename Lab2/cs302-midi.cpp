06786edabb9302dbf5702890ec0c9793f33dd66f
Audaris Blades <audaris@gmail.com>
2017-09-24 16:18:44 -0400

    Tidy up

--- Lab2/cs302-midi.cpp
+++ Lab2/cs302-midi.cpp
@@ -19,62 +19,42 @@ Convert MIDI Event/Note Files
 void CS302_Midi::el_to_nd()
 {
 	nd = new NDMap;
-	  
-	//Holds Note Damper Events
-	  //Indexed by pitch
+
+	//Keeps track of the total time to insert into ND
+	double timeSinceStart = 0; 
+
+	//Temp pointers
 	vector<ND*> tmp(128);
-	  
-	//Create Vector of size 128
 	ND *note = NULL;
 	Event *event = NULL;
 	ND *damper = NULL;
-	//Set Vector Values to NULL
 
-	//Keeps track of the total time to insert into ND
-	double timeSinceStart = 0;
-
-	//Loop through EventList
 	for(EventList::iterator it = el->begin(); it != el->end(); it++){
-		//Current event in the iteration
 		event = *it;
-		//Converts time and adds it to timeSinceStart
 		timeSinceStart += (event->time/480.0);
 
 	 	//Encountered On Event
 	  	if(event->key == 'O'){	
-	      	//Create ND
-		  
 			note = new ND;
 		  	//Stores time relative to total time
 			note->start = timeSinceStart;
-			//value of pitch
 			note->pitch = event->v1;
-			//value of volume
 			note->volume = event->v2;
-			//Add key to note
 			note->key = 'N';
 			//Store note based off pitch(index)
 			tmp[note->pitch] = note;
-
-
-
-
 	  	}
-	    //Enocountered OFF Event
+
+	    //Encountered OFF Event
 	    if(event->key == 'F'){
-			//Get from ND tmp vec
 			note = tmp[event->v1];
-	      	// Set stop time
 			note->stop = timeSinceStart;
-	      	//Insert into ND
 			nd->insert(std::pair<double, ND*>(note->start, note));
-			//Set location in temp vector to NULL
 			tmp[event->v1] = NULL;
 	    }
 
 	    //DamperDown Event
 	    if(event->key == 'D' && event->v1 == 1){
-			 
 			damper = new ND;
 			damper->key = 'D';
 			damper->start = timeSinceStart;
@@ -86,8 +66,6 @@ void CS302_Midi::el_to_nd()
 			damper = tmp[0];
 			damper->stop = timeSinceStart;
 			nd->insert(std::pair<double, ND*>(damper->start, damper));
-			//damper = NULL;
-			//test
 		}
 	}
 }
@@ -95,21 +73,21 @@ void CS302_Midi::el_to_nd()
 void CS302_Midi::nd_to_el()
 {
 
-	enum eventType {OFF = 0, ON = 3, DU = 1 , DD = 2};
+	enum eventType {OFF, DU, DD, ON};
+
   	el = new EventList;
-  	//Temp map to store contents of nd
-	
-	map<double, multimap<int, Event *> >tmp;
-	Event* du = NULL;
+
+  	//Temp pointers
+	map<int, multimap<int, Event *> >tmp;
 	ND *note = NULL;
 	Event* onEvent = NULL;
 	Event* offEvent = NULL;
 	Event* dd = NULL;
+	Event* du = NULL;
+
   	for(NDMap::iterator ndit = nd->begin(); ndit != nd->end(); ndit++){
 		note = ndit->second;
 
-		//Dealing with a note
-		//every note must have an on/off event
 		if(note->key == 'N'){
 			//Create On Event
 			onEvent = new Event;
@@ -118,65 +96,54 @@ void CS302_Midi::nd_to_el()
 			onEvent->v1 = note->pitch;
 			onEvent->time = rint(note->start*480);
 			tmp[onEvent->time].insert(pair<int, Event *>(ON,onEvent));
+
 			//Create Off Event
 			offEvent = new Event;
 			offEvent->key = 'F';
 			offEvent->v1 = note->pitch;
 			offEvent->time = rint(note->stop*480);
-
-			//Fill in information
 			tmp[offEvent->time].insert(pair<int, Event *>(OFF,offEvent));
 		}
+
 		//Dealing with a damper
 		if(note->key == 'D'){
-			//Create new DamperDown
+			//Create DamperDown
 			dd = new Event;
 			dd->key = 'D';
 			dd->v1 = 1;
 			dd->time = rint(note->start*480);
 			tmp[dd->time].insert(pair<int, Event *>(DD, dd));
-			//Create new DamperUp
+
+			//Create DamperUp
 			du = new Event;
 			du->key = 'D';
 			du->v1 = 0;
 			du->time = rint(note->stop*480);
-			//Fill in info
 			tmp[du->time].insert(pair<int, Event *>(DU, du));
-
 		}
 	}
 
-
-	//MAP IS SORTED BY THE TIME IN WHICH EVENTS OCCUR ABSOLUTELY
-	//Iterates through the map
 	Event *event = NULL;
-	int previousTime = 0;
 	bool timeSet = false;
-	for(std::map<double, multimap<int, Event *> >::iterator mit = tmp.begin(); mit != tmp.end(); mit++){
-		//Creates reference to the multimap at each time
-		//Iterates through the Multimap starting from second unit
+	int previousTime = 0;
+
+	for(std::map<int, multimap<int, Event *> >::iterator mit = tmp.begin(); mit != tmp.end(); mit++){
 		timeSet = false;
 		for(std::multimap<int, Event*>::iterator mmit = mit->second.begin(); mmit != mit->second.end(); mmit++){
 			event = NULL;
 			event = mmit->second;
 			
-			//set time of inital event if iterator
+			//set time of first in map
 			if(timeSet == false ){
-				//The current absolute time
 				int currentTime = mit->first;
-				
 				event->time = currentTime - previousTime;
 				timeSet = true;
 				previousTime = currentTime;
 			}
 			else
 				event->time = 0;
-			//Events in same map occur at same time
-			//Convert the time
-			
-			//Push to EL
+
 			el->push_back(event);
-			
 		}
 	}
 }
