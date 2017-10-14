8b34880d992ac689136a96cad8a7421185758589
Audaris Blades <audaris@gmail.com>
2017-10-14 19:14:12 -0400

    Fix repo issues

---
+++ Lab4/sb-analyze.cpp
@@ -0,0 +1,26 @@
+/*Audaris Blades
+ * Lab 5
+ * Superball
+ */
+class Superball{
+	public:
+		analyze_superball();
+};
+
+Superball::analyze_superball(){
+	
+};
+
+int main(argc, char *argv){
+	//rows
+	argv[1];
+	
+	//cols
+	argv[2];
+
+	//minscoresize
+	argv[3];
+
+	//colors
+	argv[4];
+}
