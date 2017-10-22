c2ec85a18ede3da28e8f54f1d31cbfcd233433d0
Audaris Blades <audaris@gmail.com>
2017-10-22 18:25:50 -0400

    saving just in case

--- Lab5/quick_2_sort.cpp
+++ Lab5/quick_2_sort.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include "sorting.h"
 #include <cmath>
+#include <iomanip>
 
 
 void printList(vector <double> &arr){
@@ -18,7 +19,10 @@ void printList(vector <double> &arr){
 }
 
 void quickSort(vector <double> &v, int left, int size, int print){
-    cout <<"S :     " << left << "    " << size << "    "; printList(v);
+    if(size > 1 && print == 1){
+    cout << setfill(' ') <<"S:" << setw(6)  << left << setw(6) << size << setw(8) << ' '; printList(v);
+ //   cout <<"S :     " << left << "    " << size << "    "; printList(v);
+    }
     if(size < 2)
         return;
 
@@ -28,6 +32,20 @@ void quickSort(vector <double> &v, int left, int size, int print){
         return;
     }
 
+    int start = left;
+    int middle = (left + size)/2;
+    int last = start + size - 1;
+
+    //Median of 3
+
+    if((v[start] > v[last]  >= v[middle]) || (v[middle] >= v[last] > v[start]))
+        swap(v[start], v[last]);
+    else if((v[start] > v[middle]  > v[last]) || (v[last] > v[middle] > v[start]))
+        swap(v[start], v[middle]);
+
+    if(print == 1){
+        cout << setfill(' ') <<"M:" << setw(6)  << left << setw(6) << size << setw(6) << v[start] << setw(2) << ' '; printList(v);
+    }
 
     int pivot = left;
 
@@ -58,79 +76,13 @@ void quickSort(vector <double> &v, int left, int size, int print){
 //    cout << "PIVOT VALUE" << high << endl;
     swap(v[left], v[high]);
     //cout << "pivot swap " << left << "and " << high << endl;
-    cout <<"P :     " << left << "    " << size << " " << high << "  "; printList(v);
-
+    //cout <<"P :     " << left << "    " << size << " " << high << "  "; printList(v);
+    if(print == 1){
+    cout << setfill(' ') <<"P:" << setw(6)  << left << setw(6) << size << setw(6) << high << setw(2) << ' '; printList(v);
+    }
 
     quickSort(v,left, (low - left) -1, 1);
     quickSort(v, low,  (left + size) - low, 1);
-   // cout << "second qSort index"  << left+ low << " " << (left + size) - low << endl;
-
-
- /*       cout <<"S :     " << left << "    " << left+size << "    "; printList(v);
-      if(size < 2)
-          return;
-
-      if(size == 2){
-          if(v[left] > v[left + 1])
-              swap(v[left],v[left + 1]);
-
-          return;
-      }
-
-
-      int pivot = left;
-      //left index
-      int low = left+1;
-      //right index
-      int high = left + size-1;
-    int end = left + size;
-
-      if(high < low)
-          return;
-
-      //while the index havent crossed
-      while(low < high){
-          //increment low pointer until it reaches the end or find element
-          //increment high pointer until it reaches the front or finds element
-          while(v[high] > v[pivot])
-              high--;
-          //     cout <<"high: "<< high << " ";
-          while(v[low] < v[pivot] && low <= high)
-              low++;
-        //  cout <<"low: "<< low << " ";
-
-
-          if(low < high){
-              swap(v[low],v[high]);
-              cout << " Swapped " << low << " and " << high << endl << endl;
-              low++;
-              high--;
-          } else
-              break;
-      }
-
-      if(low == high)
-          low++;
-
-      //swap pivot
-      swap(v[left], v[high]);
-      cout <<"P :     " << left << "    " << left+size  << " " << high << "  "; printList(v); cout << endl;
-      cout << "recurse on " << left << " " << high - 1<<endl;
-      cout << "recurse on " << high << " " << size<<endl;
-      quickSort(v, left, high - 1, 1);
-      quickSort(v, high+1, size, 1);
-
-      swap(v[pivot],v[high]);
-      cout << "low: " << low << " high: " << high <<endl;
-      cout <<"P :     " << start << "    " << end << " " << high << "  "; printList(v);
-      //left half recurse
-      quickSort(v, start, end - low, 1);
-      //right half recurse
-      quickSort(v, low + 1, high, 1);
-
-*/
-
-
 }
 
 
@@ -138,10 +90,10 @@ void quickSort(vector <double> &v, int left, int size, int print){
 
 
 void sort_doubles(vector<double> &v, int print){
-   // recursiveSort(v, 0, v.size(), 1);
+   quickSort(v, 0, v.size(), print);
 };
 
-int main()
+/*int main()
 {
     int size;
     int print;
@@ -156,7 +108,7 @@ int main()
     v.push_back(7.70);  v.push_back(9.85);  v.push_back(7.70);  v.push_back(2.93);
     v.push_back(9.85);  v.push_back(2.93);  v.push_back(2.93);  v.push_back(9.85);*/
 
-    v.push_back(5.77);  v.push_back(5.26);  v.push_back(6.49);  v.push_back(0.43);
+  /*  v.push_back(5.77);  v.push_back(5.26);  v.push_back(6.49);  v.push_back(0.43);
     v.push_back(6.09);  v.push_back(4.95);  v.push_back(3.15);  v.push_back(3.46);
     v.push_back(7.55);  v.push_back(6.82);  v.push_back(6.69);  v.push_back(3.62);
 
@@ -164,8 +116,8 @@ int main()
     v.push_back(4.95);  v.push_back(3.15);  v.push_back(3.46);  v.push_back(7.55);  v.push_back(6.82);
     v.push_back(6.69);  v.push_back(3.62);*/
 
-
+/*
     quickSort(v,0, v.size(), 1);
 
     return 0;
-}
+}*/
