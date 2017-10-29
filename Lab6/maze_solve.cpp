dae6f8dcf161cf2395efb93d94d6b8c04e15de7f
Audaris Blades <audaris@gmail.com>
2017-10-29 02:59:38 -0400

    Narrowed down issue to recursive dfs

--- Lab6/maze_solve.cpp
+++ Lab6/maze_solve.cpp
@@ -6,28 +6,24 @@
 #include <string>
 #include <iostream>
 #include <cmath>
-
+#include <stdlib.h>
+#include <algorithm>
 using namespace std;
 //Node structure
 class Node{
-    //ids are ints
     public:
-        //Indexed by (N S W E) (0 1 2 3)
-        vector<int> adjList;
+        vector <int> adjList;
         bool visited;
         int id;
+        //Indexed by (N S W E) (0 1 2 3)
 		//Set default adj list
-		Node(){
-			adjList.resize(4, -1);
-            visited = false;
-		}
 };
 
 class Graph{
     //All nodes
     //Indexed by id
     public:
-        vector<Node *> graph;
+        vector<Node *> nodes;
         bool dfs(int node);
         /*g.node(2) gives node at index*/
 
@@ -37,32 +33,38 @@ class Graph{
 //Maze end makes code more portable
 bool Graph::dfs(int node){
 
-	//Adjecency
-	//0- North  1-South 2-East 3-West
-	if(graph[node]->visited)
-        return 0;
 
-	//Base Case Reached start
-	if(graph[node]->id == 0)
-		return 1;
-	//DFS Base Case
-	if(dfs(node)){
-        cout << "PATH " << graph[node]->id;
-	}
+        Node *n;
+        n = nodes[node];
+        if (n->visited)
+            return false;
 
-	graph[node]->visited = true;
+        if (node == nodes.size()) {
+            cout << "Path " << node << endl;
+        }
 
-    if(graph[node]->adjList[0] != -1)
-        dfs(graph[node]->adjList[0]);
+    if(true) {
+        cout << "PATH " << node << endl;
+    }
 
-    if(graph[node]->adjList[1] != -1)
-        dfs(graph[node]->adjList[1]);
+        n->visited = true;
 
-    if(graph[node]->adjList[2] != -1)
-        dfs(graph[node]->adjList[2]);
+        //DFS Base Case
 
-    if(graph[node]->adjList[3] != -1)
-        dfs(graph[node]->adjList[3]);
+        //Adjecency
+        //0- North  1-South 2-East 3-West
+
+        //Base Case Reached start
+
+
+        for (int i = 0; i < nodes[node]->adjList.size(); i++)
+            if (!nodes[n->adjList[i]]->visited)
+                return dfs(n->adjList[i]);
+
+    return false;
+    }
+    //return false;
+
 	//visited return
  //base case first index
     //solve maze backwards
@@ -78,99 +80,102 @@ bool Graph::dfs(int node){
     //print
     //if(dfs  returns 1 on the path to start)
     //return 0
-}
+
 
 int main(){
     //read in rows/ col
     //store in variable
+    string s;
     int rows;
     int cols;
+    Node * n;
     Graph g;
-    cin.ignore();
+    cin >> s;
+    cout << s << " ";
     cin >> rows;
-    cin.ignore();
+    cout << rows << " ";
+    cin >> s;
+    cout << s << " ";
     cin >> cols;
+    cout << cols << endl;
+
+    for(int i = 0; i < rows*cols;i++){
+        n = new Node;
+        n->id = i;
+        n->visited = false;
+        g.nodes.push_back(n);
+    }
     //initialize node vector
-    for(int i = 0; i < cols; i++){
-        for(int j = 0; j < rows; j++){
-
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
             int index = i*cols + j;
-            Node n;
-
+           // n.adjList.resize(4, -1);
+            //n->visited = false;
+           // cout << "Index: " << index;
             //Look North
-            if((i*(cols - 1) + j > 0)){
-                n.adjList[0] = i*(cols - 1) + j;
+            if((i - 1)*cols + j >= 0){
+                g.nodes[index]->adjList.push_back((i - 1)*cols + j);
+              //  cout << " North: "<< (i-1)*cols + j;
             }
 
             //Look South
-            if((i*(cols + 1) + j < rows*cols)){
-                n.adjList[1] = i*(cols + 1);
+            if((i+1)*(cols) + j < rows*cols){
+                g.nodes[index]->adjList.push_back((i+1)*cols +j);
+            //    cout << " South: " << (i +1)*cols +j;
             }
 
             //Look West
             if(index - 1 >= 0 && j != 0){
-                n.adjList[2] = index - 1;
+                g.nodes[index]->adjList.push_back(index - 1);
+             //   cout << " West: " << index - 1;
             }
 
             //Look East
             if(index + 1 < rows*cols && j != cols - 1){
-                n.adjList[3] = index + 1;
+                g.nodes[index]->adjList.push_back(index + 1);
+              //  cout << " East: " << index + 1;
             }
-
-            //Add node to vector
-            g.graph.push_back(&n);
+           // cout << endl;
         }
     }
-    string s;
+
     while(cin >> s){
-        //
+        cin.clear();
         int w1,w2;
-        cin >> s;
+        //cin >> s;
+        cout << s << " ";
         //First location of wall
         cin >> w1;
+        cout << w1 << " ";
         //Second location of wall
         cin >> w2;
+        cout << w2 << endl;
 
-        //Wall Detection
-        int adj = abs(w2 - w1);
-        //Wall is vertical
-        if(adj == 1){
-            //Adjust Respective Adj List
-            if(w2 > w1){
-                g.graph[w2]->adjList[3] = -1;
-                g.graph[w1]->adjList[4] = -1;
-            }
-            else if(w1 > w2){
-                g.graph[w1]->adjList[3] = -1;
-                g.graph[w2]->adjList[4] = -1;
-            }
-
+        //Wall Detection;
+        vector<int>::iterator it1, it2;
+        //Look for w1 in w2 and remove it
+        it2 = find(g.nodes[w2]->adjList.begin(), g.nodes[w2]->adjList.end(), w1);
+        if(it2 != g.nodes[w2]->adjList.end()){
+            g.nodes[w2]->adjList.erase(it2);
+            cout << "ERASING " << w1 << " FROM " << w2 <<endl;
         }
-        //Horizontal wall
-        if(adj == cols){
-            if(w2 > w1){
-                //Remove Upper Wall from W2
-                g.graph[w2]->adjList[0] = -1;
-                g.graph[w1]->adjList[1] = -1;
-            }
-            else if(w2 < w1){
-                g.graph[w2]->adjList[1] = -1;
-                g.graph[w1]->adjList[0] = -1;
-            }
-
+        //Look for w2 in w1
+        it1 = find(g.nodes[w1]->adjList.begin(), g.nodes[w1]->adjList.end(), w2);
+        if(it1 != g.nodes[w1]->adjList.end()){
+            g.nodes[w1]->adjList.erase(it1);
+            cout << "ERASING " << w2 << " FROM " << w1 <<endl;
         }
-
-
     }
-    //store in variable
-    //Make instance of graph
-    //Make rxc nodes
-    //push into g.nodes
-    //initialize nodes
-    //(can fill in vectors we can fill in graph)
-    //fix each nodes adj list
-    //initialize adj -1
-    //make each index in adj list represent a direction [_ _ _ _] (N S W E)
-    //print back out rows and cols
-    g.dfs(rows*cols -1);
+
+  /*  for(int i = 0; i < g.nodes.size(); i++){
+        cout << "Node: " << i;
+        for(int j = 0; j < g.nodes[i]->adjList.size();j++){
+            cout<< " adj: " << g.nodes[i]->adjList[j];
+        }
+        cout << endl;
+    }*/
+
+    g.dfs(0);
+
+
 }
