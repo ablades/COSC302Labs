6755b2f3f979c3c0d6f290cfaccd7f5a8588eec0
Audaris Blades <audaris@gmail.com>
2017-11-05 12:25:34 -0500

    Added Test Files. Updated City_Map

--- Lab7/city_map.cpp
+++ Lab7/city_map.cpp
@@ -6,18 +6,19 @@
 #include "city_map.h"
 #include <map>
 #include <vector>
+#include <iterator>
 
 City_Map::City_Map(){
-    Intersection *IS;
-    City_Map() *c = new City_Map;
-    c->all.clear();
+    Intersection *IS = new Intersection();
+    City_Map *c = new City_Map;
 
     double input;
-    while(cin >> input){
-        IS = new Intersection;
-        IS->street = input;
-        cin >> input;
-        IS->avenue = input;
+    int st, ave;
+    int stmax, avemax;
+    while(cin >> st){
+        IS->street = st;
+        cin >> ave;
+        IS->avenue = ave;
         cin >> input;
         IS->x = input;
         cin >> input;
@@ -26,8 +27,44 @@ City_Map::City_Map(){
         IS->green[0] = input;
         cin >> input;
         IS->green[1] = input;
-        c->all.push_back(&IS);
+        c->all.push_back(IS);
+
+        //First Intersection
+        if(st == 0 & ave == 0)
+            c->first = IS;
+        //Set Last intersection point
+        if(st > stmax || ((stmax == st) && ave >= avemax)){
+            stmax = st;
+            avemax = ave;
+            c->last = IS;
+        }
+        IS = new Intersection();
     }
+    //set initial size of temp vector
+    vector< vector<Intersection *> > tmp(stmax+1, vector<Intersection *>(avemax +1));
+
+    //Store contents of all into temp in proper position
+        for(list<Intersection *>::iterator itr = c->all.begin(); itr !=  c->all.end(); itr++)
+            tmp[(*itr)->street][(*itr)->avenue] = *itr;
+
+    for(int i = 0; i < stmax; i++)
+        for(int j = 0; j < avemax; i++){
+            //Look Up
+            if(i > 0 && (j % 5 == 0 || j % 2 != 0 || j == avemax)){
+
+            }
+            //Look Down
+            if(i < stmax && (j % 2 == 0 || j % 5 == 0 || j == avemax)){
+                //create road segment
+
+            }
+            //Look left
+            if( j > 0 && (i % 5 == 0)){}
+            //Look right
+            if( j < avemax && (i % 5 == 0)){}
+        }
+
+
 
 }
 
