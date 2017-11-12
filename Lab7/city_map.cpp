700fb902c66ba7e0dcfb2d52b1d27ecf15e46d43
Audaris Blades <audaris@gmail.com>
2017-11-12 14:10:43 -0500

    Best Case Dijkstra Complete

--- Lab7/city_map.cpp
+++ Lab7/city_map.cpp
@@ -31,7 +31,6 @@ City_Map::City_Map(){
         IS->green[1] = gAve;
         IS->backedge = NULL;
         IS->best_time = -1;
-
         //First Intersection
         if(st == 0 & ave == 0)
             first = IS;
@@ -58,6 +57,7 @@ City_Map::City_Map(){
     Road_Segment* rs;
     for(int i = 0; i < tmp.size(); i++){
         for(int j = 0; j < tmp[i].size(); j++){
+
             //Look Up
             if(i > 0 && (j % 5 == 0 || j % 2 != 0 || j == aveMax)){
                 rs = new Road_Segment();
@@ -68,6 +68,7 @@ City_Map::City_Map(){
                 rs->distance = sqrt(pow(tmp[i][j]->x - tmp[i-1][j]->x,2) + pow(tmp[i][j]->y - tmp[i-1][j]->y, 2));
                 tmp[i][j]->adj.push_back(rs);
             }
+
             //Look Down
             if(i < strMax && (j % 2 == 0 || j % 5 == 0 || j == aveMax)){
                 rs = new Road_Segment;//create road segment
@@ -76,6 +77,17 @@ City_Map::City_Map(){
                 rs->from = tmp[i][j];
                 rs->to = tmp[i+1][j];
                 rs->distance = sqrt(pow(tmp[i][j]->x - tmp[i+1][j]->x,2) + pow(tmp[i][j]->y - tmp[i+1][j]->y, 2));
+                tmp[i][j]->adj.push_back(rs);
+            }
+
+            //Look right
+            if( j < aveMax && (i % 5 == 0 || i % 2 == 0)){
+                rs = new Road_Segment;
+                rs->type = STREET;
+                rs->number = i;
+                rs->from = tmp[i][j];
+                rs->to = tmp[i][j+1];
+                rs->distance = sqrt(pow(tmp[i][j]->x - tmp[i][j+1]->x,2) + pow(tmp[i][j]->y - tmp[i][j+1]->y, 2));
                 tmp[i][j]->adj.push_back(rs);
             }
 
@@ -89,77 +101,115 @@ City_Map::City_Map(){
                 rs->distance = sqrt(pow(tmp[i][j]->x - tmp[i][j-1]->x,2) + pow(tmp[i][j]->y - tmp[i][j-1]->y, 2));
                 tmp[i][j]->adj.push_back(rs);
             }
-            //Look right
-            if( j < aveMax && (i % 5 == 0 || i % 2 == 0)){
-                rs = new Road_Segment;
-                rs->type = STREET;
-                rs->number = i;
-                rs->from = tmp[i][j];
-                rs->to = tmp[i][j+1];
-                rs->distance = sqrt(pow(tmp[i][j]->x - tmp[i][j+1]->x,2) + pow(tmp[i][j]->y - tmp[i][j+1]->y, 2));
-                tmp[i][j]->adj.push_back(rs);
-            }
        }
     }
 }
 
 double City_Map::Dijkstra(int avg_best_worst){
+    first->best_time = 0;
     bfsq.insert(make_pair(0, first));
-
+    bool flag = false;
     //Queue
     while(bfsq.size()){
         Intersection * IS;
         Road_Segment * rs;
-        double curTime = 0;
-        double travelTime =-1;
+       // int waitTime = 0;
 
         IS = bfsq.begin()->second;
-        curTime = bfsq.begin()->first;
+      //  IS->bfsq_ptr = bfsq.begin();
         //Remove node from queue
         bfsq.erase(bfsq.begin());
-        IS->best_time = curTime;
 
-        //loop through adj list and check if visited
+     //   IS->best_time = 0;
+
+        //loop through adj list
         for(list<class Road_Segment *>::iterator itr = IS->adj.begin(); itr != IS->adj.end(); itr++){
             rs = *itr;
+            int red = rs->to->green[1];
+            int green = rs->to->green[0];
+            int smallestDelay;
+            int waitTime = 0;
+            double newTime = 0;
 
             //If intersection has been visited it will have a best time
-            if(rs->to->best_time != -1)
-                continue;
+            //cout << rs->to->best_time << "Best Time" << endl;
+         //   if(rs->to->best_time != -1)
+           //     continue;
 
-            //Calculate time to node
-            travelTime = rs->distance / 30 * 3600;
+           /* if(red < green)
+                smallestDelay = red;
+            else
+                smallestDelay = green;*/
 
 
-            if(travelTime < rs->to->best_time || rs->to->best_time == -1){
+                //Average Case
+          /*  if(avg_best_worst == 'A'){
 
-                //If the intersection is in the queue remove it
-                for(multimap<double, Intersection *>::iterator itr = bfsq.begin(); itr != bfsq.end(); itr++)
-                    if(itr->second == rs->to)
-                        bfsq.erase(itr);
+                if(rs->type == STREET)
+                    waitTime = pow(red,2)/(2*(green + red));
+                else if(rs->type == AVENUE)
+                    waitTime = pow(green, 2)/(2*(green + red));
+            }
+                //Worst Case
+            if(avg_best_worst == 'W'){
 
-                rs->to->best_time = curTime + travelTime;
+                if(rs->type == STREET)
+                    waitTime = rs->to->green[1];
+                else if(rs->type == AVENUE)
+                    waitTime = rs->to->green[0];
+            }*/
+
+            //cout << "Current Time" << curTime  << endl;
+
+
+
+
+
+            //time to next intersection
+
+
+                newTime += (rs->distance / 30 * 3600)  + IS->best_time + waitTime;
+           // cout << "Travel Time" << travelTime <<endl;
+            //Remove and add to path
+            if(rs->to->best_time == -1 || newTime < rs->to->best_time ){
+            /*&& waitTime <= smallestDelay*/
+                if(rs->to->best_time == -1)
+                    rs->to->best_time += 1;
+
+                //If the best time is in the intersection remove it and insert next best time
+              //  if(rs->to->best_time != -1){
+                   for(multimap<double, Intersection *>::iterator it = bfsq.begin(); it != bfsq.end(); it++){
+                        if(it->second == rs->to ){
+                          //  cout << "Here";
+                            bfsq.erase(it);
+                        }
+                    }
+
+
+             //   cout << rs->to->best_time << endl;
+                rs->to->best_time = newTime;
+               // cout << rs->to->best_time << endl;
                 rs->to->backedge = rs;
-                rs->to->bfsq_ptr = bfsq.insert(make_pair(curTime + travelTime, rs->to));
+                bfsq.insert(make_pair(rs->to->best_time , rs->to));
             }
         }
     }
+
     double time = 0;
     //Start at last intersection
 
 
     //Traverse back edges from last node
-    //While we are not at the first node
-    Intersection * lastIS;
-    lastIS = last;
-    while(lastIS->backedge != NULL){
-        //Assign it to next intersection
-        path.push_front(lastIS->backedge);
-        lastIS = lastIS->backedge->from;
-    }
+    Road_Segment* p;
+    p = last->backedge;
 
+    while(p != NULL){
+        path.push_front(p);
+        p = p->from->backedge;
+    }
+   // cout << path.size();
     for(list<class Road_Segment *>::iterator itr = path.begin(); itr != path.end(); itr++){
-        time += (*itr)->from->best_time;
+        time += (*itr)->to->best_time;
     }
 
     //Returns the total time
