ffab0959b812d567e4e95b668e448cd402dbb2c6
Audaris Blades <audaris@gmail.com>
2017-12-03 19:46:48 -0500

    Okay

--- LabB/dond.cpp
+++ LabB/dond.cpp
@@ -1,122 +1,62 @@
 /*Audaris Blades
- * Dice or No Dice
- * Calculates Probablity*/
-
-#include <vector>
+ * Dice or No Dice Calcuate the probablilty
+*/
+#include <map>
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <limits>
+#include <map>
+#include <iostream>
+#include <sstream>
 
 using namespace std;
 
-double sum(vector<double> p){
-    double s = 0;
-    for(int i = 0; i < p.size(); i++)
-        s += p[i];
+bool adj(int n, int m);
 
-    return s;
-};
+double dond(int sides, int rolls, int lastRoll, map <pair <int, int>, double> &cache) {
 
-int main(int argc, char *argv[]){
-    double sides, turns, lastRoll;
-        if(argc != 4)
-            exit(1);
+    double result = 0;
 
-        sides = argv[1];
-        //number of rolls left
-        turns = argv[2];
-        //last roll
-        lastRoll = argv[3];
+    pair <int, int> index(rolls, lastRoll);
+    // Check the cache for value
+    if(cache.count(index))
+        return cache[index];
 
-    vector<double> prob(sides, 0);
-
-    //Initialize vector
-    for(int i = 0; i < sides; i++){
-        //edge probability
-        if(i == 0 || i == sides -1)
-            prob[i] = (s-2/s);
-        else
-            prob[i] = (s-3/s); // has a greater and lower value
-    }
-
-    //All rolls are legal
-    if(turns == 1){
-        //Edge case
-        if(lastRoll == 0 || lastRoll == sides - 1)
-            cout << s-2/s << endl;
-        else if(lastRoll == -1)
-            cout << 1;
-        else{
-            cout << s-3/s << endl;
-        }
-    }
+    // Base case.
+    if (rolls == 0 || lastRoll == -1)
+        return 1;
 
 
-    if(turns > 1){
 
-        if(lastRoll != -1){
-
-            prob[lastRoll] = 0;
-            if(lastRoll == 0)
-                prob[1] = 0;
-            else if(lastRoll == sides -1)
-                prob[sides-1] = 0;
-            else{
-                prob[lastRoll - 1] = 0;
-                prob[lastRoll + 1] = 0;
-            }
+    // Sum of the probabilities
+    for(int i = 0; i < sides; i++)
+        if (!adj(i, lastRoll)){
+            result += dond(sides, rolls - 1, i, cache);
+           // cout << result << endl;
         }
 
-        //Multiplies the probablity vector by the turn number
-        for(int i = 1; i < turns ;i++){
-            for(int j = 0; j < prob.size(); j++)
-                prob[j] *= (1/sides);
-        }
-    }
-
-
-
-
+    // Return prob
+    return cache[index] = result / (double) sides;
 }
 
-/*class Dice{
-    double s;
-    vector< vector <double > > cache;
-    double rc(int t, int last);
-};
+int main(int argc, char** argv) {
+    map <pair <int, int>, double> cache;
+    int sides, rolls, lastRoll;
 
+    //arg count check
+    if (argc != 4)
+        exit(-1);
 
-int main(int argc, char *argv[]){
-    int t,lr;
-    Dice d;
+    sides = atoi(argv[1]);
+    rolls = atoi(argv[2]);
+    lastRoll = atoi(argv[3]);
+    cout << dond(sides, rolls, lastRoll, cache) << endl;
 
-    if(argc != 4)
-        exit(1);
-
-        d.s = argv[1];
-        //number of rolls left
-        t = argv[2];
-        //last roll
-        lr = argv[3];
-
-    return rc(t, lr);
-  //  }
-}
-//calculate probabilities
-double Dice::rc(int t, int last) {
-    long long sum = 0;
-  //Check if we've called rc before with values
-    if(cache[t][last] != -1)
-        return cache[t][last];
-
-
-    //Probablity for one role left
-    //Store in cache
-
-    if(last == -1 || last == 0){
-        for(int i = 0; i < s; i++)
-            sum += rc(t-1,i);
-
-        return sum;
-    }
-
-
+    return 0;
 }
 
+//Check adj
+ bool adj(int n, int m) {
+    return (n == m+1 || n == m || n+1 == m);
+}
---
+++ LabB/spellpath.cpp
@@ -0,0 +1,250 @@
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <list>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct Point
+{
+    int x, y;
+};
+
+list<Point> findPath(vector<vector <int> > grid, vector<vector <int> > mat, int i, int j) {
+    list<Point> path;
+
+    int M, N;
+    M = mat.size();
+    N = mat[0].size();
+
+    Point pt;
+    pt.x = i;
+    pt.y = j;
+    path.push_front(pt);
+
+    while (grid[i][j] != 0)
+    {
+        //up
+        if (i > 0 && grid[i][j] - 1 == grid[i - 1][j])
+        {
+            pt.x = i - 1;
+            pt.y = j;
+            path.push_front(pt);
+            i--;
+        }
+        //left
+        if (j > 0 && grid[i][j] - 1 == grid[i][j - 1])
+        {
+            pt.x = i;
+            pt.y = j - 1;
+            path.push_front(pt);
+            j--;
+        }
+        //Down
+       /* if (i+1 < M && grid[i][j] - 1 == grid[i + 1][j])
+        {
+            pt.x = i + 1;
+            pt.y = j;
+            path.push_front(pt);
+            i++;
+        }*
+        //right
+        if (j+1 < N && grid[i][j] - 1 == grid[i][j + 1])
+        {
+            pt.x = i;
+            pt.y = j + 1;
+            path.push_front(pt);
+            j++;
+        }*/
+
+        if((j+1 < N && i+1 < M) && grid[i][j] - 1 == grid[i+1][j+1]){
+            pt.x = i + 1;
+            pt.y = j + 1;
+            path.push_front(pt);
+            j++;
+            i++;
+        }
+        if((j > 0 && i+1 < M) && grid[i][j] - 1 == grid[i+1][j-1]){
+            pt.x = i + 1;
+            pt.y = j - 1;
+            path.push_front(pt);
+            j--;
+            i++;
+        }
+
+        if((j+1 < N && i > 0) && grid[i][j] - 1 == grid[i-1][j+1]){
+            pt.x = i - 1;
+            pt.y = j + 1;
+            path.push_front(pt);
+            j++;
+            i--;
+        }
+
+        if((j > 0  && i > 0) && grid[i][j] - 1 == grid[i-1][j-1]){
+            pt.x = i - 1;
+            pt.y = j - 1;
+            path.push_front(pt);
+            j--;
+            i--;
+        }
+    }
+
+    return path;
+}
+
+// Function to find maximum length
+void findSequence(vector<vector <int> > mat)
+{
+    int M, N;
+    M = mat.size();
+    N = mat[0].size();
+
+    // table to store results of subproblems
+    vector<vector <int> > lookup(M, vector<int>(N, 0));
+    // initialize by 0
+   // memset(lookup, 0, sizeof lookup);
+
+    // stores maximum length
+    int max_len = 0;
+
+    // store cordinates to snake's tail
+    int max_row = 0;
+    int max_col = 0;
+
+    // fill the table in bottom-up fashion
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            // do except for (0, 0) cell
+            if (i || j) {
+                // look above
+                if (i > 0 && abs(mat[i - 1][j] - mat[i][j]) == 1) {
+                    lookup[i][j] = max(lookup[i][j], lookup[i - 1][j] + 1);
+
+                    if (max_len < lookup[i][j])
+                    {
+                        max_len = lookup[i][j];
+                        max_row = i, max_col = j;
+                    }
+                }
+
+                /*// look down
+                if (i+1 < M && abs(mat[i + 1][j] - mat[i][j]) == 1) {
+                    lookup[i][j] = max(lookup[i][j], lookup[i + 1][j] + 1);
+
+                    if (max_len < lookup[i][j])
+                    {
+                        max_len = lookup[i][j];
+                        max_row = i, max_col = j;
+                    }
+                }*/
+
+                // look left
+                if (j > 0 && abs(mat[i][j - 1] - mat[i][j]) == 1) {
+                    lookup[i][j] = max(lookup[i][j], lookup[i][j - 1] + 1);
+
+                    if (max_len < lookup[i][j]) {
+                        max_len = lookup[i][j];
+                        max_row = i, max_col = j;
+                    }
+                }
+
+                // look right
+                /*
+                if (j+1 < N && abs(mat[i][j + 1] - mat[i][j]) == 1) {
+                    lookup[i][j] = max(lookup[i][j], lookup[i][j + 1] + 1);
+
+                    if (max_len < lookup[i][j]) {
+                        max_len = lookup[i][j];
+                        max_row = i, max_col = j;
+                    }
+                }*/
+
+                //Check weird bounds
+                //column is even
+                if(j % 2 == 0){
+                    //check r+1, c+1
+                    if((j+1 < N && i+1 < M) && abs(mat[i + 1][j + 1] - mat[i][j]) == 1){
+                        lookup[i][j] = max(lookup[i][j], lookup[i+1][j+1] + 1);
+
+                        if (max_len < lookup[i][j]) {
+                            max_len = lookup[i][j];
+                            max_row = i, max_col = j;
+                        }
+                    }
+
+                    //check r+1 c -1
+                    if((j > 0 && i+1 < M) && abs(mat[i + 1][j - 1] - mat[i][j]) == 1){
+                        lookup[i][j] = max(lookup[i][j], lookup[i+1][j-1] + 1);
+
+                        if (max_len < lookup[i][j]) {
+                            max_len = lookup[i][j];
+                            max_row = i, max_col = j;
+                        }
+                    }
+                }
+
+                //col is odd
+                if(j % 2 != 0){
+                    //check r-1, c+1
+                    if((j+1 < N && i > 0) && abs(mat[i - 1][j + 1] - mat[i][j]) == 1){
+                        lookup[i][j] = max(lookup[i][j], lookup[i-1][j+1] + 1);
+
+                        if (max_len < lookup[i][j]) {
+                            max_len = lookup[i][j];
+                            max_row = i, max_col = j;
+                        }
+                    }
+
+                    //check r-1 c-1
+                    if((j > 0  && i > 0) && abs(mat[i - 1][j - 1] - mat[i][j]) == 1){
+                        lookup[i][j] = max(lookup[i][j], lookup[i-1][j-1] + 1);
+
+                        if (max_len < lookup[i][j]) {
+                            max_len = lookup[i][j];
+                            max_row = i, max_col = j;
+                        }
+                    }
+
+                }
+           }
+        }
+    }
+
+    //cout << "Maximum length" << max_len << endl;
+
+    // find maximum length path
+    list<Point> path = findPath(lookup, mat, max_row, max_col);
+    cout << path.size() << endl;
+    /*cout << "PATH:";
+    for (list<Point>::iterator it = path.begin(); it != path.end(); it++)
+        cout << endl << mat[it->x][it->y]  << it->x << ", " << it->y ;*/
+}
+
+
+// Driver code
+int main(int argc, char **argv)
+{
+    int i = 0;
+    vector< vector <int> > board;
+    string row;
+    while(getline(cin, row)){
+        
+        //Add new column
+        vector<int> col;
+        board.push_back(col);
+        for(int j = 0; j < row.size(); j++){
+      //      cout << row[j];
+            board[i].push_back(row[j]);
+        }
+     //   cout << endl;
+
+        i++;
+    }
+
+    findSequence(board);
+
+    return 0;
+}
